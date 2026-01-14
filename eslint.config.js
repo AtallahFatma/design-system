e8eda2bcffbe82119da57b94645bb13ee6078d73
Fatma Atallah <fatma.atallah@gmail.com>
2026-01-14 23:24:45 +0100

    Added husky pre-commit hook to run tests and lint-staged before commits

--- eslint.config.js
+++ eslint.config.js
@@ -7,11 +7,19 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from "eslint-plugin-react";
+import unusedImports from "eslint-plugin-unused-imports";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "unused-imports": unusedImports,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -21,6 +29,38 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      // React
+      "react/react-in-jsx-scope": "off", // Vite + React 17+
+      "react/jsx-uses-react": "off",
+
+      // Hooks
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      // 🔥 Unused imports (this is the important part)
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
     },
   },
+  prettier,
 ])
